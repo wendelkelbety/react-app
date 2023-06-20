f4bacd9ae0f69d4b1b05b935a2bb815d5ddf2b77
Wendel <wendelkleberty@gmail.com>
2023-06-20 08:24:53 -0300

    react-app

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Dashboard from './components/dashboard'
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard 
+      title="Meu Painel de Controle"
+      copyrighttext="Meu WebSite ©"
+      />
+    </div>
+  );
+}
+
+export default App;
